97b8b3ad9e3e3e10c6e3391288746176fd4afa09
Avadhoot Sutar <avadhootsutar2103@gmail.com>
2025-01-21 18:22:56 +0530

    do nothing

--- BasicPython/mutableDefaultParams.py
+++ BasicPython/mutableDefaultParams.py
@@ -2,7 +2,7 @@ from typing import List, Optional
 class Student:
     # never assign a default value to parameters  like grades default  is empty []
     # def __init__(self, name:str, grades: List[int] = []): # this is so bad
-    def __init__(self, name:str, grades: Optional[List[int]] = None): # this is so bad
+    def __init__(self, name:str, grades: Optional[List[int]]=None ): # this is so bad
         self.name = name
         self.grades = grades or []
     
