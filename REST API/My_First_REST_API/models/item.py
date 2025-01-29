2ab6bc6cefb4a9d77e46fa45f8e987f6090c7182
Avadhoot Sutar <avadhootsutar2103@gmail.com>
2025-01-29 13:39:12 +0530

    item_name = unique = fl

--- REST API/My_First_REST_API/models/item.py
+++ REST API/My_First_REST_API/models/item.py
@@ -5,7 +5,7 @@ class ItemModel(db.Model): #type: ignore
     __tablename__ = "items"
 
     item_id = db.Column(db.Integer, primary_key= True)
-    item_name = db.Column(db.String(80), unique=True, nullable=False)
+    item_name = db.Column(db.String(80), unique=False, nullable=False)
     item_price= db.Column(db.Float(precision=2), unique=False, nullable=False)
     store_id = db.Column(db.Integer, db.ForeignKey("stores.store_id"), unique=False, nullable= False)
     # grab me a store object with th above store_id
